aea01bf9fdf14ad8b2ce9cf12573d53192d99d01
Vinicius <vinijaguare@gmail.com>
2024-12-08 12:08:02 -0300

    create rectangle class

---
+++ include/rectangle.h
@@ -0,0 +1,33 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <bits/stdc++.h>
+
+class Rectangle{
+    GLfloat width;
+    GLfloat height;
+    GLfloat x;
+    GLfloat y;
+    GLfloat z;
+    GLfloat R; 
+    GLfloat G;
+    GLfloat B;
+
+public:
+    Rectangle(
+        GLfloat width,
+        GLfloat height,
+        GLfloat x,
+        GLfloat y,
+        GLfloat z,
+        GLfloat R, 
+        GLfloat G,
+        GLfloat B
+    );
+
+    void Desenha();
+};
+
+#endif//RECTANGLE_H
---
+++ src/rectangle.cpp
@@ -0,0 +1,29 @@
+#include "../include/rectangle.h"
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+void Rectangle::Desenha(){
+    glColor3f(R, G, B);
+
+    glPushMatrix();
+        glTranslatef(x, y, 0);
+        glBegin(GL_QUADS);
+            glVertex3f(0, 0, z);
+            glVertex3f(0, -height, z);
+            glVertex3f(width, -height, z);
+            glVertex3f(width, 0, z);
+        glEnd();
+    glPopMatrix();
+}
+
+Rectangle::Rectangle(GLfloat width, GLfloat height, GLfloat x, GLfloat y, GLfloat z, GLfloat R, GLfloat G, GLfloat B){
+    // std::cout <<  " width " << width <<  " height " << height <<  " x " << x <<  " y " << y <<  " z " << z << std::endl;
+    this->width = width;
+    this->height = height;
+    this->x = x;
+    this->y = y;
+    this->z = z;
+    this->R = R;
+    this->G = G;
+    this->B = B;
+}
