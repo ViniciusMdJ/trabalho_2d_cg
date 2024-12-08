fe7d3de6f7b34c721cd392d6f94cf572d37f0c91
Vinicius <vinijaguare@gmail.com>
2024-12-08 12:08:32 -0300

    create arena class

---
+++ include/arena.h
@@ -0,0 +1,20 @@
+#ifndef ARENA_H
+#define ARENA_H
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <bits/stdc++.h>
+
+#include "rectangle.h"
+
+class Arena{
+    Rectangle* backgroud;
+    std::vector<Rectangle> obstacles;
+
+public:
+    Arena(GLfloat width, GLfloat height, GLfloat x, GLfloat y);
+    void addObstacles(Rectangle rect);
+    void Desenha();
+};
+
+#endif//ARENA_H
---
+++ src/arena.cpp
@@ -0,0 +1,24 @@
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <bits/stdc++.h>
+#include <iostream>
+
+#include "../include/rectangle.h"
+#include "../include/arena.h"
+
+Arena::Arena(GLfloat width, GLfloat height, GLfloat x, GLfloat y){
+    std::cout << width << " " << height << " " << x << " " << y << std::endl;
+
+    backgroud = new Rectangle(width, height, x, y, 0, 0.0, 0.0, 1.0);
+}
+
+void Arena::addObstacles(Rectangle rect){
+    obstacles.push_back(rect);
+}
+
+void Arena::Desenha(){
+    backgroud->Desenha();
+    for(auto i : obstacles){
+        i.Desenha();
+    }
+}
