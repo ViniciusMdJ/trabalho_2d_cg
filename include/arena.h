66577aa04e9c216eaca72462888c9aaee10b6213
Vinicius <vinijaguare@gmail.com>
2025-01-14 16:11:21 -0300

    update Arena

--- include/arena.h
+++ include/arena.h
@@ -7,16 +7,23 @@
 
 #include "rectangle.h"
 #include "player.h"
+#include "bullet.h"
 
 class Arena{
     Rectangle* backgroud;
     std::vector<Rectangle> obstacles;
+    std::list<Bullet> bullets;
+    std::list<Player> enemies;
 
 public:
     Arena(GLfloat width, GLfloat height, GLfloat x, GLfloat y);
     void addObstacles(Rectangle rect);
     void Draw();
     void verifyCollision(Player &player);
+    void addBullet(Bullet bullet);
+    void addEnemy(Player enemy);
+    bool bulletCheck(const Bullet& value, Player &player);
+    void updateArena(Player &player, GLdouble timeDiff);
 };
 
 #endif//ARENA_H
--- src/arena.cpp
+++ src/arena.cpp
@@ -5,6 +5,7 @@
 
 #include "../include/rectangle.h"
 #include "../include/arena.h"
+#include "../include/utils.h"
 
 Arena::Arena(GLfloat width, GLfloat height, GLfloat x, GLfloat y){
     // std::cout << width << " " << height << " " << x << " " << y << std::endl;
@@ -16,9 +17,19 @@ void Arena::addObstacles(Rectangle rect){
     obstacles.push_back(rect);
 }
 
+void Arena::addEnemy(Player enemy){
+    enemies.push_back(enemy);
+}
+
 void Arena::Draw(){
     backgroud->Draw();
     for(auto i : obstacles){
+        i.Draw();
+    }
+    for(auto i : bullets){
+        i.Draw();
+    }
+    for(auto i : enemies){
         i.Draw();
     }
 }
@@ -33,7 +44,42 @@ void Arena::verifyCollision(Player &player){
     for(auto i : obstacles){
         v += i.Collides(rect);
     }
+    for(auto i : enemies){
+        v += i.getBoundingBox().Collides(rect);
+    }
 
     // std::cout << v.getComponent(0) << " " << v.getComponent(1) << std::endl;
     player.Move(v); 
+}
+
+void Arena::addBullet(Bullet bullet){
+    bullets.push_back(bullet);
+}
+
+bool Arena::bulletCheck(const Bullet& value, Player &player){
+    for(auto i : obstacles){
+        if(checkColision(value, i)){
+            return true;
+        }
+    }
+    auto i = enemies.begin();
+    while(i != enemies.end()){
+        if(checkColision(value, i->getBoundingBox())){
+            enemies.erase(i);
+            return true;
+        }
+        i++;
+    }
+    if(checkColision(value, player.getBoundingBox())){
+        return true;
+    }
+    return !checkColision(value, *backgroud);
+}
+
+void Arena::updateArena(Player &player, GLdouble timeDiff){
+    for(auto &i : bullets){
+        i.Move(timeDiff * 2 * INC_MOVE);
+    }
+    bullets.remove_if([this, &player](const Bullet& value) { return bulletCheck(value, player); });
+    verifyCollision(player);
 }
